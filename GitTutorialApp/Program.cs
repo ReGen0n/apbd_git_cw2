747047e6d6d63573b242c14cf3e51a4cc52979af
ReGen0n <Nazarik2005@gmail.com>
2026-03-14 15:40:05 +0100

    Add Max Calculation

--- GitTutorialApp/Program.cs
+++ GitTutorialApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿int[] numbers = {2, 4, 6, 8};
 
 double average = numbers.Average();
+int max = numbers.Max();
 
-Console.WriteLine("Average:" + average);
+Console.WriteLine("Average: " + average);
+Console.WriteLine("Max: " + max);
